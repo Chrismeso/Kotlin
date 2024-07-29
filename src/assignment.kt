c94d94c05c1a4491a68f9dd5c781371071413fe2
njeriviolet <njeriviolet01@gmail.com>
2024-07-29 10:53:49 +0300

    commit

--- src/assignment.kt
+++ src/assignment.kt
@@ -1,4 +1,22 @@
+import java.util.Scanner
 
+fun main() {
+    var read = Scanner(System.`in`)
+    println("Enter letter")
+    val letter = readln()
+
+
+    if(letter!=null && letter.length==1 && letter[0].isLetter()){
+        when(letter){
+            "a","e","i","o","u" -> println("$letter is a vowel")
+            else -> println("$letter is a consonant")
+
+       }
+    }
+    else {
+        println("Enter valid input")
+    }
+}
 
 
 
---
+++ src/when2.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var number = 2
+    var result = when(number){
+        1 -> "one"
+        2 -> "two"
+        3 -> "three"
+        4 -> "four"
+        else -> "Invalid number"
+
+    }
+    println("Number is $result")
+
+}
